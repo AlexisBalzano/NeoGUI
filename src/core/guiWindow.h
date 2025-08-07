cd642c8297bd4da2c67bacc03820b33c9d7311ac
Alexis <alexis.balzano@epfl.ch>
2025-08-07 10:40:22 +0200

    Creation of guiWindow class

---
+++ src/core/guiWindow.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <optional>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+namespace neogui {
+
+class GuiWindow {
+public:
+    GuiWindow(unsigned int width, unsigned int height, const std::string& title, const std::wstring& parentTitle);
+
+    void setVisible(bool visible);
+
+    bool isVisible() const { return show_; }
+    bool isOpen() const { return window_.isOpen(); }
+    void close() { window_.close(); }
+
+    void processEvents();
+    void updateDrag();
+    void render();
+
+private:
+    sf::RenderWindow window_;
+    sf::CircleShape shape_;
+    unsigned int width_, height_;
+    bool show_;
+
+#ifdef _WIN32
+    HWND hwnd_;
+    bool dragging_;
+    POINT clickOffset_{};
+#endif
+};
+} // namespace neogui
